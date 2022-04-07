efa57b9ea2299b30a98f0632b91d57818a3d2c02
Hubert Thieriot <hubert@energyandcleanair.org>
2022-04-07 13:15:00 +0800

    -adding ongoing plots

--- api.R
+++ api.R
@@ -5,9 +5,9 @@ api.get_voyages <- function(date_from=NULL){
     url <- paste0(url, sprintf("date_from=%s&", strftime(date_from, "%Y-%m-%d")))
   }
 
-  shipments <- jsonlite::fromJSON(url)$data %>%
+  jsonlite::fromJSON(url)$data %>%
     filter(departure_iso2=="RU",
-           arrival_iso2!="RU") %>%
+           (arrival_iso2!="RU" | is.na(arrival_iso2))) %>%
     mutate_at(c("departure_date_utc","arrival_date_utc"), lubridate::as_date) %>%
     mutate(arrival_country = countrycode(arrival_iso2, "iso2c", "country.name"))
 }
@@ -15,7 +15,9 @@ api.get_voyages <- function(date_from=NULL){
 api.get_voyages_sf <- function(date_from=NULL){
   # Can be quite slow
   url <- sprintf("https://fossil-shipment-tracker.ew.r.appspot.com/v0/voyage?format=geojson&nest_in_data=False")
-  tryCatch({geojsonsf::geojson_sf(url(url))},
+  tryCatch({geojsonsf::geojson_sf(url(url)) %>%
+      filter(departure_iso2=="RU",
+             (arrival_iso2!="RU" | is.na(arrival_iso2)))},
            error=function(e){
              return(NULL)
            })
@@ -44,4 +46,25 @@ api.get_voyage_points <- function(voyage_id){
   positions <- jsonlite::fromJSON(url)$data %>%
     mutate_at(c("date_utc"), lubridate::as_date) %>%
     sf::st_as_sf(coords=c("lon","lat"))
+}
+
+api.get_portcalls <- function(date_from=NULL){
+  url <- "https://fossil-shipment-tracker.ew.r.appspot.com/v0/portcall?"
+
+  if(!is.null(date_from)){
+    url <- paste0(url, sprintf("date_from=%s&", strftime(date_from, "%Y-%m-%d")))
+  }
+
+  jsonlite::fromJSON(url)$data
+}
+
+
+api.get_ports <- function(){
+  url <- "https://fossil-shipment-tracker.ew.r.appspot.com/v0/port?"
+  jsonlite::fromJSON(url)$data
+}
+
+api.get_ships <- function(){
+  url <- "https://fossil-shipment-tracker.ew.r.appspot.com/v0/ship?"
+  jsonlite::fromJSON(url)$data
 }
--- server/tab_plots.R
+++ server/tab_plots.R
@@ -21,13 +21,12 @@
 
 
 # # Reactive Elements --------------------------------------
-
-
-output$plot_voyages_lines <- renderPlotly({
+plot_data <- reactive({
 
   voyages <- voyages()
   req(input$arrival_or_departure)
   req(voyages)
+  add_ongoing <- input$add_ongoing
 
   commodities <- c("lng"="LNG",
                    "crude_oil"="Crude Oil",
@@ -37,48 +36,65 @@ output$plot_voyages_lines <- renderPlotly({
                    "bulk_notcoal"="Bulk (ex. coal)",
                    "coal"="Coal")
 
-  d <- voyages %>%
-    mutate(quantity=dwt) %>%
-    # mutate(quantity = ifelse(unit=="tonne", quantity/1000, quantity),
-           # unit = ifelse(unit=="tonne", "thousand tonne", unit)) %>%
+  d <- voyages
+
+  if(add_ongoing){
+    d <- d %>% filter(status %in% c("ongoing", "completed")) %>%
+      mutate(arrival_iso2=ifelse(status=="ongoing", destination_iso2, arrival_iso2))
+  }else{
+    d <- d %>% filter(status %in% c("completed"))
+  }
+
+  d <- d %>%
+    mutate(quantity = ifelse(unit=="tonne", quantity/1000, quantity),
+    unit = ifelse(unit=="tonne", "thousand tonne", unit)) %>%
     mutate(commodity = recode(commodity, !!!commodities)) %>%
-    # mutate(commodity = sprintf("%s - %s", commodity, unit)) %>%
-    filter(status=="completed") %>%
-    filter(!grepl("bulk", commodity, ignore.case = T))
+    filter(!grepl("bulk|lpg|oil_or_ore", commodity, ignore.case = T))
+
 
   d$date <- d[,input$arrival_or_departure]
-  d["destination_region"] = countrycode::countrycode(d$arrival_iso2, "iso2c", "country.name")
-  d[d$arrival_iso2 %in% codelist$iso2c[codelist$eu28=="EU"], "destination_region"] <- "EU28"
+  d$destination_region = countrycode::countrycode(d$arrival_iso2, "iso2c", "country.name")
+  d[d$arrival_iso2 %in% setdiff(codelist$iso2c[codelist$eu28=="EU"], NA), "destination_region"] <- "EU28"
 
-
-  identified_regions <- c("EU28",  "China", "United States", "Japan", "India", "Turkey", "South Korea", "Taiwan", "Malaysia", "Egypt", "Morocco")
+  identified_regions <- c("EU28",  "China", "United States", "Japan", "India", "Turkey", "South Korea", "Taiwan", "Malaysia", "Egypt", "Unknown")
+  d[is.na(d$destination_region), "destination_region"] <- "Unknown"
   d[!d$destination_region %in% identified_regions, "destination_region"] <- "Other"
 
   # d %>% group_by(arrival_iso2, destination_region) %>% summarise_at("quantity", sum, na.rm=T) %>% arrange(desc(quantity)) %>% View()
-  top_importers <- d %>% group_by(destination_region) %>% summarise(quantity=sum(quantity, na.rm=T)) %>% arrange(desc(quantity)) %>% pull(destination_region)
-
-  add_trailing <- function(x) paste0(x, "   ")  # Tweak to prevent cut legend...
-  d$destination_region <- factor(add_trailing(d$destination_region), levels=add_trailing(c(setdiff(top_importers, "Other"),"Other")))
+  top_importers <- d %>% group_by(destination_region) %>% summarise(dwt=sum(dwt, na.rm=T)) %>% arrange(desc(dwt)) %>% pull(destination_region)
+  d$destination_region <- factor(d$destination_region, levels=c(setdiff(top_importers, "Other"),"Other"))
 
   d <- d %>%
-    group_by(destination_region, date, commodity) %>%
-    summarise(quantity=sum(quantity, na.rm=T)) %>%
+    group_by(destination_region, date, commodity, unit) %>%
+    summarise_at(c("dwt","quantity"), sum, na.rm=T) %>%
     ungroup()
 
   filler <- tibble(date=seq.Date(min(d$date), max(d$date), by="day")) %>%
-    tidyr::crossing(d %>% ungroup() %>% distinct(destination_region, commodity))
+    tidyr::crossing(d %>% ungroup() %>% distinct(destination_region, commodity, unit))
 
+  # Add 0s
+  d <- d %>% full_join(filler) %>%
+    mutate(quantity = tidyr::replace_na(quantity, 0),
+           dwt = tidyr::replace_na(dwt, 0))
+
+  return(d)
+})
+
+
+output$plot_voyages_lines <- renderPlotly({
+
+  d <- plot_data()
+  req(d)
 
   # Add 0s and rolling average
-  d <- d %>% full_join(filler) %>%
-    mutate(quantity = tidyr::replace_na(quantity, 0)) %>%
-    rcrea::utils.running_average(7, vars_to_avg = "quantity")
+  d <- d %>%
+    rcrea::utils.running_average(7, vars_to_avg = c("quantity","dwt"))
 
   colourCount = length(unique(d$commodity))
   getPalette = colorRampPalette(brewer.pal(12, "Paired"))
 
   plt <- ggplot(d) +
-    geom_line(aes(date, quantity, col=commodity),
+    geom_line(aes(date, dwt, col=commodity),
              stat="identity") +
     facet_wrap(~destination_region, nrow=3) +
     rcrea::theme_crea() +
@@ -94,36 +110,17 @@ output$plot_voyages_lines <- renderPlotly({
 })
 
 
-output$plot_voyages <- renderPlotly({
+output$plot_voyages_bars <- renderPlotly({
 
-  voyages <- voyages()
-  req(input$arrival_or_departure)
-  req(voyages)
+  d <- plot_data()
+  req(d)
 
-  commodities <- c("lng"="LNG",
-                   "crude_oil"="Crude Oil",
-                   "oil_products"="Oil Products",
-                   "oil_or_chemical"="Oil or Chemical",
-                   "bulk"="Bulk (ex. coal)",
-                   "bulk_notcoal"="Bulk (ex. coal)",
-                   "coal"="Coal")
-
-  d <- voyages %>%
-    mutate(quantity = ifelse(unit=="tonne", quantity/1000, quantity),
-      unit = ifelse(unit=="tonne", "thousand tonne", unit)) %>%
-    mutate(commodity = recode(commodity, !!!commodities)) %>%
-    mutate(commodity = sprintf("%s - %s", commodity, unit),
-           quantity = quantity)
-
-  d$date <- d[,input$arrival_or_departure]
-  d["destination_region"] = d$arrival_iso2
-  d[d$arrival_iso2 %in% codelist$iso2c[codelist$eu28=="EU"], "destination_region"] <- "EU28"
-
-  identified_regions <- c("EU28", "Japan", "China", "United States", "India", "Korea")
-  d[!d$destination_region %in% identified_regions, "destination_region"] <- "Other"
+  d <- d %>%
+    mutate(commodity = sprintf("%s - %s", commodity, unit))
 
   add_trailing <- function(x) paste0(x, "   ")  # Tweak to prevent cut legend...
   d$destination_region <- factor(add_trailing(d$destination_region), levels=rev(add_trailing(c(identified_regions,"Other"))))
+
   d <- d %>%
     group_by(destination_region, date, commodity, unit) %>%
     summarise(quantity=sum(quantity, na.rm=T))
